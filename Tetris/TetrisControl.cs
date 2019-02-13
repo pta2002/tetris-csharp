0e7d724dd0c9b18b61bbe04a4697c3462fb4e958
rooti <rootitooter@protonmail.com>
2019-02-13 21:45:12 +0000

    draw dot grid

--- Tetris/TetrisControl.cs
+++ Tetris/TetrisControl.cs
@@ -19,6 +19,7 @@ namespace Tetris
             InitializeComponent();
 
             Board = new TetrisBoard();
+            DoubleBuffered = true;
         }
 
         protected override void OnPaint(PaintEventArgs pe)
@@ -26,11 +27,23 @@ namespace Tetris
             base.OnPaint(pe);
             pe.Graphics.Clear(Color.Black);
 
+            Brush b = new SolidBrush(Color.DarkGray);
+            for (int x = 0; x < 20; x++)
+            {
+                for (int y = 0; y < 20; y++)
+                {
+                    pe.Graphics.FillRectangle(b, x * 25 - 1, y * 25 - 1, 2, 2);
+                }
+            }
+
             foreach (Block block in Board.GetBlocks())
             {
-                Brush b = new SolidBrush(block.Color);
+                b.Dispose();
+                b = new SolidBrush(block.Color);
                 pe.Graphics.FillRectangle(b, block.X * 25, block.Y * 25, 25, 25);
             }
+
+            b.Dispose();
         }
 
         public void Tick()
